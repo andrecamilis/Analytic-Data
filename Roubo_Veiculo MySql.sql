ce45c2c35d3c77448b658d77e3604ad1e9293921
Musty <119085798+andrecamilis@users.noreply.github.com>
2025-05-30 12:32:31 -0300

    seccond commit

---
+++ Roubo_Veiculo MySql.sql
@@ -0,0 +1,14 @@
+What could this code mean?
+
+This code block checks if the user has granted permission to send notifications.
+
+The notification bar is the top section of your smartphone screen. It shows you what's happening—like if someone liked your photo on Instagram or sent a message for you on WhatsApp.
+
+If you scroll down, you'll see the raffle() method.
+
+This method will created a random number and show in screen 
+
+
+ 
+
+	
